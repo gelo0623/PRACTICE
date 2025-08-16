bd442ad2ac837866189d0b1aa53da4c9ec02aa40
Gelo <mapanis@gbox.adnu.edu.ph>
2025-08-16 22:42:33 +0800

    Create ily.cpp
    for her

---
+++ ily.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string a = "I";
+    string b = "Love"; 
+    string c = "You";
+
+ int n;
+
+    cout << "Enter first Name: ";
+    cin >> n;
+    cout << a << " " << endl;
+
+    cout << "Enter Midddle Name: ";
+    cin >> n;
+    cout << b << " " << endl;
+
+    cout << "Enter Last Name: ";
+    cin >> n;
+    cout << c << " " << endl;
+
+    cout << "The word is" << a << " " << b << " " << c << " " << "Juliana Nicole P. Ramos" << endl;
+
+    return 0;
+}
